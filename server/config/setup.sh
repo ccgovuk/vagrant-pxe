19af990c6c778f35b4bab480d66103ea5b8096ea
Chris Clayton <chris.clayton@digital.homeoffice.gov.uk>
2018-10-23 11:45:15 +0100

    correct typo

--- server/config/setup.sh
+++ server/config/setup.sh
@@ -2,17 +2,17 @@
 
 ### DHCPD configuration
 apt-get install -y isc-dhcp-server
-cp /config/ressources/dhcpd.conf /etc/dhcp/dhcpd.conf
-cp /config/ressources/isc-dhcp-server /etc/default/isc-dhcp-server
+cp /config/resources/dhcpd.conf /etc/dhcp/dhcpd.conf
+cp /config/resources/isc-dhcp-server /etc/default/isc-dhcp-server
 systemctl restart isc-dhcp-server
 
 ### TFTPD configuration
 apt-get install -y tftpd-hpa
-cp /config/ressources/tftpd-hpa /etc/default/tftpd-hpa
+cp /config/resources/tftpd-hpa /etc/default/tftpd-hpa
 mkdir /tftpboot
-cp -R /config/ressources/pxelinux.cfg /tftpboot
-cp -R /config/ressources/syslinux /tftpboot
-cp /config/ressources/pxelinux.0 /tftpboot
+cp -R /config/resources/pxelinux.cfg /tftpboot
+cp -R /config/resources/syslinux /tftpboot
+cp /config/resources/pxelinux.0 /tftpboot
 systemctl restart tftpd-hpa
 
 ### PXE configuration
